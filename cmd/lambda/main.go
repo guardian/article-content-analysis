81b6e1380f5055607900ea926e6c0e55f62cc400
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2019-04-05 11:35:27 +0100

    pass path to lambda

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -3,11 +3,22 @@ package main
 import (
 	"article-entity-analysis/internal"
 	"github.com/aws/aws-lambda-go/lambda"
+	"golang.org/x/net/context"
 )
 
+type Path struct {
+	Path string `json:"path"`
+}
+
+func HandleRequest(ctx context.Context, path Path) (string, error) {
+	entities := internal.GetEntitiesForPath(path.Path)
+	var res = ""
+	for _, entity := range entities {
+		res += entity.GoString()
+	}
+	return res, nil
+}
+
 func main() {
-	lambda.Start(func() (string error) {
-		internal.GetEntitiesForPath("/film/2019/apr/04/amazon-claims-woody-allen-sabotaged-films-with-metoo-comments")
-		return "test", nil
-	})
+	lambda.Start(HandleRequest)
 }
--- go.mod
+++ go.mod
@@ -10,5 +10,5 @@ require (
 	github.com/tidwall/gjson v1.2.1
 	github.com/tidwall/match v1.0.1 // indirect
 	github.com/tidwall/pretty v0.0.0-20190325153808-1166b9ac2b65 // indirect
-	golang.org/x/net v0.0.0-20190403144856-b630fd6fe46b // indirect
+	golang.org/x/net v0.0.0-20190403144856-b630fd6fe46b
 )

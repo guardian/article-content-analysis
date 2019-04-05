df982df8fd6cb7a0040feb8e2a07d4a97da2fff7
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2019-04-05 09:16:02 +0100

    return error

--- cmd/lambda/main.go
+++ cmd/lambda/main.go
@@ -6,8 +6,8 @@ import (
 )
 
 func main() {
-	lambda.Start(func() string {
+	lambda.Start(func() (string error) {
 		internal.GetEntitiesForPath("/film/2019/apr/04/amazon-claims-woody-allen-sabotaged-films-with-metoo-comments")
-		return "test"
+		return "test", nil
 	})
 }

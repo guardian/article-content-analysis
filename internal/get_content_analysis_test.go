e20a3f4caada1dd527e13bbe769d94db03ef491d
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2019-04-11 14:43:59 +0100

    add ability to run for date range

--- internal/get_content_analysis_test.go
+++ internal/get_content_analysis_test.go
@@ -7,7 +7,22 @@ import (
 )
 
 func TestGetContentAnalysis(t *testing.T) {
-	res, err := GetContentAnalysis("/uk-news/2019/apr/11/meghan-and-harry-want-to-celebrate-birth-of-baby-in-private", "test")
+	res, err := GetContentAnalysisForPath("/uk-news/2019/apr/11/meghan-and-harry-want-to-celebrate-birth-of-baby-in-private", "test")
+
+	if err != nil {
+		t.Error(err)
+	} else {
+		res, err := json.Marshal(res)
+		if err != nil {
+			t.Error(err)
+		} else {
+			fmt.Println(string(res))
+		}
+	}
+}
+
+func TestGetContentAnalysisForDateRange(t *testing.T) {
+	res, err := GetContentAnalysisForDateRange("2019-04-10", "2019-04-11", "test")
 
 	if err != nil {
 		t.Error(err)
--- internal/services/capi.go
+++ internal/services/capi.go
@@ -3,10 +3,12 @@ package services
 import (
 	"article-content-analysis/internal/models"
 	"encoding/json"
+	"fmt"
 	"github.com/pkg/errors"
 	"github.com/tidwall/gjson"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 func GetArticleFieldsFromCapi(path string, apiKey string) (*models.Content, error) {
@@ -32,4 +34,48 @@ func GetArticleFieldsFromCapi(path string, apiKey string) (*models.Content, erro
 		return nil, errors.Wrap(articleFieldsError, "could not parse response from CAPI")
 	}
 	return articleFields, nil
+}
+
+func GetArticleFieldsFromCapiForDateRange(fromDate string, endDate string, apiKey string) (*models.CapiSearchResponse, error) {
+	var fullResults = new(models.CapiSearchResponse)
+	var reachedEndOfResults = false
+	var pageIndex = 1
+
+	for reachedEndOfResults == false {
+		var capiReponse = new(models.CapiSearchResponse)
+
+		urlPrefix := "https://content.guardianapis.com/search"
+		urlSuffix := "?api-key=" + apiKey + "&from-date=" + fromDate + "&to-date=" + endDate + "&page-size=200" + "&page=" + strconv.Itoa(pageIndex)
+		url := urlPrefix + urlSuffix
+		fmt.Println(url)
+		resp, err := http.Get(url)
+		if err != nil {
+			return nil, errors.Wrap(err, "no response from CAPI")
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not read body test")
+		}
+
+		//TODO: validate response
+		fields := gjson.Get(string(body), "response").Raw
+		fieldsBytes := []byte(fields)
+		capiResponseError := json.Unmarshal(fieldsBytes, &capiReponse)
+		if capiResponseError != nil {
+			return nil, errors.Wrap(capiResponseError, "could not parse response from CAPI")
+		}
+		if capiReponse.Status == "error" {
+			reachedEndOfResults = true
+		} else {
+			for _, result := range capiReponse.Results {
+				result.Id = "/" + result.Id
+				fullResults.Results = append(fullResults.Results, result)
+				pageIndex++
+
+			}
+		}
+
+	}
+	return fullResults, nil
 }

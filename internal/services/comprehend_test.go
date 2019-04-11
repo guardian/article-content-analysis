c92d483345a0f5d8910d5d1bc7ab6edcbbcaeb17
Tom Forbes <tom.forbes@theguardian.com>
2019-04-11 15:55:52 +0100

    more tabs

--- internal/services/comprehend_test.go
+++ internal/services/comprehend_test.go
@@ -1,18 +1,18 @@
 package services
 
 import (
-	"fmt"
-	"testing"
+    "fmt"
+    "testing"
 )
 
 func TestGetEntitiesFromPath(t *testing.T) {
-	res, err := GetEntitiesFromPath("/uk-news/2019/apr/11/julian-assange-arrested-at-ecuadorian-embassy-wikileaks")
+    res, err := GetEntitiesFromPath("/uk-news/2019/apr/11/julian-assange-arrested-at-ecuadorian-embassy-wikileaks")
 
-	if err != nil {
-		t.Error(err)
-	} else {
-		for _, entity := range res {
-			fmt.Println(entity.GoString())
-		}
-	}
+    if err != nil {
+        t.Error(err)
+    } else {
+        for _, entity := range res {
+            fmt.Println(entity.GoString())
+        }
+    }
 }
